397b38c74db7a2e349160c92fa163b1704e02e4b
mo <245994626@qq.com>
2021-11-06 11:20:34 +0800

    add the code of flag module and the init function.

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"blogcluster"
+	"flag"
 	"time"
 )
+
+var remoteAddr, browserBrand string
+var updateInterval int
+
+func init() {
+	flag.StringVar(&remoteAddr,
+		"addr",
+		"http://localhost:9222",
+		"browser remote debugging url")
+	flag.StringVar(&browserBrand,
+		"browser",
+		"edge",
+		"brower brand, like chrome and edge")
+	flag.IntVar(&updateInterval,
+		"updateInterval",
+		3,
+		"interval (second) for browser to update the tag list")
+	flag.Parse()
+}
 
 func main() {
 	parser := blogcluster.NewCommonParser()
